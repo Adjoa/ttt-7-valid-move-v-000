ea3fb26778b228da30fcb9d1c2523da5a6c27a3b
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-14 20:11:58 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?(board, index) || (!index.between?(1, 9))
-    return false
-  else
+  if index.between?(1, 9) && !position_taken?(board, index)
     return true
   end
 end
